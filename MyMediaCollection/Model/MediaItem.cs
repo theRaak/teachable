3b98aa6c7f8241097637bbccca7fd97c647bd9dd
Brian Raak <bjraak@uw.edu>
2021-09-11 17:32:17 -0500

    still working through ch02

---
+++ MyMediaCollection/Model/MediaItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyMediaCollection.Enums;
+
+namespace MyMediaCollection.Model
+{
+    public class MediaItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ItemType MediaType { get; set; }
+        public Medium MediumInfo { get; set; }
+        public LocationType Location { get; set; }
+    }
+}
---
+++ MyMediaCollection/Model/Medium.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMediaCollection.Model
+{
+    public class Medium
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ItemType MediaType { get; set; }
+    }
+}
