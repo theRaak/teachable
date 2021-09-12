7fef1e15049312cee144999abf5f452f560121d5
Brian Raak <bjraak@uw.edu>
2021-09-11 22:16:14 -0500

    continuing with ch02 work

--- MyMediaCollection/MainPage.xaml.cs
+++ MyMediaCollection/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using MyMediaCollection.Model;
+using MyMediaCollection.Enums;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,14 +25,44 @@ namespace MyMediaCollection
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public MainPage()
+		private IList<MediaItem> _items { get; set; }
+		private bool _isLoaded;
+		public MainPage()
         {
             this.InitializeComponent();
-        }
-
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            //myButton.Content = "Clicked";
-        }
-    }
+			PopulateData();
+		}
+		public void PopulateData()
+		{
+			if (_isLoaded) return;
+			_isLoaded = true;
+			var cd = new MediaItem
+			{
+				Id = 1,
+				Name = "Classical Favorites",
+				MediaType = ItemType.Music,
+				MediumInfo = new Medium { Id = 1, MediaType = ItemType.Music, Name = "CD" }
+			};
+			var book = new MediaItem
+			{
+				Id = 2,
+				Name = "Classic Fairy Tales",
+				MediaType = ItemType.Book,
+				MediumInfo = new Medium { Id = 2, MediaType = ItemType.Book, Name = "Book" }
+			};
+			var bluRay = new MediaItem
+			{
+				Id = 3,
+				Name = "The Mummy",
+				MediaType = ItemType.Video,
+				MediumInfo = new Medium { Id = 3, MediaType = ItemType.Video, Name = "Blu Ray" }
+			};
+			_items = new List<MediaItem>
+			{
+				cd,
+				book,
+				bluRay
+			};
+		}
+	}
 }
